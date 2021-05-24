6aadbd19f78417234529e3516fde46efad1af3dd
@an280420 <angit280420@gmail.com>
2021-05-25 00:13:26 +0500

    Add post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_24_185915) do
+ActiveRecord::Schema.define(version: 2021_05_24_190324) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,20 @@ ActiveRecord::Schema.define(version: 2021_05_24_185915) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "posts", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "title", null: false
+    t.text "content"
+    t.string "image"
+    t.string "time"
+    t.bigint "category_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_posts_on_category_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.integer "role", default: 0
     t.string "email", default: "", null: false
@@ -40,4 +54,6 @@ ActiveRecord::Schema.define(version: 2021_05_24_185915) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "categories"
+  add_foreign_key "posts", "users"
 end
