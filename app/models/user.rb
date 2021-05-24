f75eefb088d32a59341acd8b8797ebb39e4595d8
@an280420 <angit280420@gmail.com>
2021-05-24 23:51:55 +0500

    Add enum role to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # теперь вы можете менять роли юзера
+  # например: user  = User.first
+  # user.role = 1 присвоим роль
+  # user.role => 'author' выведем роль
+  enum role: [:standard, :author, :admin]
 end
