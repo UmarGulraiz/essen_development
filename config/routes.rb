98ba01b6180f89f3492c7e0b5aeb00692c1a48b3
@an280420 <angit280420@gmail.com>
2021-05-31 00:31:31 +0500

    Add resurse admin

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,15 @@ Rails.application.routes.draw do
   root 'posts#index'
 
   resources :post_ingredients
-  resources :posts
+  resources :posts #, only: [:show, :index]
   resources :ingredients
-  resources :categories
+  resources :categories, only: [:show, :index]
   devise_for :users
   resources :users
+
+  namespace :admin do
+    resources :categories
+    resources :posts
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
