d7e6b37b4aa49678cdc37e6757dfb9cfafb0c08b
@an280420 <angit280420@gmail.com>
2021-06-07 07:39:44 +0500

    Add permit parametr and display all exist ingredients

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -65,6 +65,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:name, :title, :content, :image, :time, :category_id, :user_id)
+      params.require(:post).permit(:name, :title, :content, :image, :time, :category_id, :user_id, ingredient_ids: [])
     end
 end
