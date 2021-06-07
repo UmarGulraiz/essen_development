9de52c0cd67fe427f22be960547c72c592713dad
@an280420 <angit280420@gmail.com>
2021-06-07 19:47:04 +0500

    Add devise and pundit settings

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,26 @@
 class ApplicationController < ActionController::Base
   include Pundit
+
+  # Настройка для работы Девайза, когда юзер правит профиль
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+  # обработчик ошибки авторизации
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized
+    # Перенаправляем юзера откуда пришел (или в корень сайта)
+    flash[:alert] = 'Вы не авторизованы'
+    redirect_to(request.referrer || root_path)
+  end
+
+  # параметры которые можно обновлять при правке профиля
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(
+      :account_update,
+      keys: [:password, :password_confirmation, :current_password]
+    )
+  end  
 end
