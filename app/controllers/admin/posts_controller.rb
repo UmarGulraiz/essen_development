6bbebc018d9ed1fdf0ca5db7985d4e911730307c
@an280420 <angit280420@gmail.com>
2021-06-07 22:52:18 +0500

    Edit views post for all users

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,6 +1,7 @@
 class Admin::PostsController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy ]
-
+  after_action :verify_authorized, only: %i[ new create edit update destroy ]
+  
   # GET /posts or /posts.json
   def index
     @posts = Post.all
@@ -13,20 +14,23 @@ class Admin::PostsController < ApplicationController
   # GET /posts/new
   def new
     @post = Post.new
+    authorize @post
     # @post = @post.post_ingredients.build
   end
 
   # GET /posts/1/edit
   def edit
+    authorize @post
   end
 
   # POST /posts or /posts.json
   def create
     @post = Post.new(post_params)
+    authorize @post
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to [:admin, @post], notice: "Post was successfully created." }
+        format.html { redirect_to @post, notice: "Post was successfully created." }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -37,9 +41,11 @@ class Admin::PostsController < ApplicationController
 
   # PATCH/PUT /posts/1 or /posts/1.json
   def update
+    authorize @post
+
     respond_to do |format|
       if @post.update(post_params)
-        format.html { redirect_to [:admin, @post], notice: "Post was successfully updated." }
+        format.html { redirect_to @post, notice: "Post was successfully updated." }
         format.json { render :show, status: :ok, location: @post }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -50,9 +56,11 @@ class Admin::PostsController < ApplicationController
 
   # DELETE /posts/1 or /posts/1.json
   def destroy
+    authorize @post
+
     @post.destroy
     respond_to do |format|
-      format.html { redirect_to admin_posts_url, notice: "Post was successfully destroyed." }
+      format.html { redirect_to posts_url, notice: "Post was successfully destroyed." }
       format.json { head :no_content }
     end
   end
@@ -65,6 +73,6 @@ class Admin::PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:name, :title, :content, :image, :time, :category_id, :user_id)
+      params.require(:post).permit(:name, :title, :content, :image, :time, :category_id, :user_id, ingredient_ids: [])
     end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: %i[ show edit update destroy ]
-  after_action :verify_authorized, only: %i[ new create edit update destroy ]
+  before_action :set_post, only: :show
   
   # GET /posts or /posts.json
   def index
@@ -11,68 +10,10 @@ class PostsController < ApplicationController
   def show
   end
 
-  # GET /posts/new
-  def new
-    @post = Post.new
-    authorize @post
-    # @post = @post.post_ingredients.build
-  end
-
-  # GET /posts/1/edit
-  def edit
-    authorize @post
-  end
-
-  # POST /posts or /posts.json
-  def create
-    @post = Post.new(post_params)
-    authorize @post
-
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to @post, notice: "Post was successfully created." }
-        format.json { render :show, status: :created, location: @post }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /posts/1 or /posts/1.json
-  def update
-    authorize @post
-
-    respond_to do |format|
-      if @post.update(post_params)
-        format.html { redirect_to @post, notice: "Post was successfully updated." }
-        format.json { render :show, status: :ok, location: @post }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /posts/1 or /posts/1.json
-  def destroy
-    authorize @post
-
-    @post.destroy
-    respond_to do |format|
-      format.html { redirect_to posts_url, notice: "Post was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_post
-      @post = Post.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def post_params
-      params.require(:post).permit(:name, :title, :content, :image, :time, :category_id, :user_id, ingredient_ids: [])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_post
+    @post = Post.find(params[:id])
+  end
 end
