119c7ecf76db7b37e5aebbd08f6313750a85d044
@an280420 <angit280420@gmail.com>
2021-06-10 11:04:21 +0500

    Edit style and links

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -13,7 +13,6 @@ class Admin::PostsController < Admin::AdminController
   # GET /posts/new
   def new
     @post = Post.new
-    # @post.post_ingredients.build
   end
 
   # GET /posts/1/edit
@@ -27,11 +26,9 @@ class Admin::PostsController < Admin::AdminController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, notice: "Post was successfully created." }
+        format.html { redirect_to [:admin, @post], notice: "Post was successfully created." }
         format.json { render :show, status: :created, location: @post }
       else
-        # @post.post_ingredients.build if @post.post_ingredients.empty?
-
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @post.errors, status: :unprocessable_entity }
       end
@@ -43,11 +40,9 @@ class Admin::PostsController < Admin::AdminController
 
     respond_to do |format|
       if @post.update(post_params)
-        format.html { redirect_to @post, notice: "Post was successfully updated." }
+        format.html { redirect_to [:admin, @post], notice: "Post was successfully updated." }
         format.json { render :show, status: :ok, location: @post }
       else
-        # @post.post_ingredients.build if @post.post_ingredients.empty?
-
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @post.errors, status: :unprocessable_entity }
       end
@@ -59,7 +54,7 @@ class Admin::PostsController < Admin::AdminController
 
     @post.destroy
     respond_to do |format|
-      format.html { redirect_to posts_url, notice: "Post was successfully destroyed." }
+      format.html { redirect_to admin_posts_url, notice: "Post was successfully destroyed." }
       format.json { head :no_content }
     end
   end
